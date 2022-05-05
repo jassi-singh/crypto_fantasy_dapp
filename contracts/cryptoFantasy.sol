b82e8d19d7c9714c4c66bcd0bbe4234ead113845
Jassi <jaswinder.dandyan2000@gmail.com>
2022-05-05 16:35:28 +0530

    Initial commit

---
+++ contracts/cryptoFantasy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+///@title Crypto Fantasy is a decentralized fantasy cricket game
+///@author  Jaswinder Singh   - https://github.com/jassi-singh
+///@notice Anybody can play fantasy cricket game in a decentralized way with the help of this contract.
+contract CryptoFantasy {
+    constructor() {}
+
+    
+}
