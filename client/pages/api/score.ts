1a01f943ba6086be03b05ad86ecdecf82c7d8abf
Jassi <jaswinder.dandyan2000@gmail.com>
2022-05-09 23:01:52 +0530

    feat (api) : change the format of the response according to smart contract need

--- client/pages/api/score.ts
+++ client/pages/api/score.ts
@@ -1,24 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios'
+import axios, { Axios, AxiosResponse } from 'axios'
 axios.defaults.headers.common = {
   'X-RapidAPI-Host': 'unofficial-cricbuzz.p.rapidapi.com',
-  'X-RapidAPI-Key': 'aac0b27d35mshf2c0872bb6b1a79p154255jsnbd7c6fd0611f',
-}
-
-type Data = {
-  score: number
+  'X-RapidAPI-Key': `${process.env.API_KEY}`,
 }
 
 export default async function getScore(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const scorecard = await axios.get(
-    'https://unofficial-cricbuzz.p.rapidapi.com/matches/get-scorecard',
-    { params: { matchId: req.body.matchId } }
-  )
-  res.status(200).json(calculateScore(scorecard.data.scorecard))
+  await axios
+    .get('https://unofficial-cricbuzz.p.rapidapi.com/matches/get-scorecard', {
+      params: { matchId: req.query.matchId },
+    })
+    .then((scorecard) => {
+      console.log(scorecard.statusText)
+      if (scorecard.status === 200)
+        res.status(200).json(calculateScore(scorecard.data.scorecard))
+      else res.status(scorecard.status).json(scorecard.statusText)
+    })
+    .catch((error) => {
+      res.status(500)
+    })
 }
 
 const calculateScore = (scorecard: any) => {
@@ -36,5 +40,12 @@ const calculateScore = (scorecard: any) => {
       scoresOfPlayers[player.id] += (player.wickets ?? 0) * 25
     })
   })
-  return scoresOfPlayers
+
+  let res: any = {},
+    idx = 0
+  for (const item in scoresOfPlayers) {
+    res[`player${idx}`] = { id: item, score: scoresOfPlayers[item] }
+    idx++;
+  }
+  return res
 }
