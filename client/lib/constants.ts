0612fb335a19b61bbdc9fbe571466fba1cc1a353
Jassi <jaswinder.dandyan2000@gmail.com>
2022-05-26 23:36:43 +0530

    feat : added abi in the next js code

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,4 +1,4 @@
-import abi from '../../smart_contract/artifacts/contracts/cryptoFantasy.sol/cryptoFantasy.json'
+import abi from './abi.json'
 
 export const contractABI = abi.abi
 export const contractAddress = '0x65f03933A53ED392217b73Fc714336a70Bb70107'
